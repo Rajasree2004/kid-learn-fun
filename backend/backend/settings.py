13e1b2c4cd50be6d6fb97492a627c146d3f002bb
rajasree.selva@gmail.com <rajasree.selva@gmail.com>
2025-02-08 14:50:24 +0000

    mongodb setup

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
+from dotenv import load_dotenv
 from pathlib import Path
+
+load_dotenv()  # Load environment variables from .env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-import os
-from dotenv import load_dotenv
-
-load_dotenv()  # Load environment variables from .env
 
 SECRET_KEY = os.getenv("SECRET_KEY", "fallback_default_secret_key")
 
@@ -86,9 +85,9 @@ DATABASES = {
     # }
     'default': {
         'ENGINE': 'djongo',
-        'NAME': 'game_fun',
+        'NAME': 'game-data',
         'CLIENT': {
-            'host': 'mongodb://localhost:27017/game_fun'
+            'host': os.getenv("MONGO_DB_STR")
         }
     }
 }
